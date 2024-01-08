9406d38727b457627a79245ea8ec7f7876be9a4b
Saad Srabon <saadsrabon2@gmail.com>
2024-01-09 03:44:00 +0600

    added daisy ui

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 
+import Footer from "../shared/Footer"
+import NavBar from "../shared/NavBar"
+
 export const MainLayout = ({children ,isDarkMode, setIsDarkMode}) => {
 console.log(isDarkMode)
 
   return (
  <>
-<Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+<NavBar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
 {children}
 <Footer setIsDarkMode={setIsDarkMode}/>
  </>
