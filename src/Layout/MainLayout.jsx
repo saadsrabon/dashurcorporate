792651cae114aa77d371a22ca11ee85cb9e45bb9
Saad Srabon <saadsrabon2@gmail.com>
2024-01-09 02:31:17 +0600

    passed props to shared component

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/prop-types */
 
 export const MainLayout = ({children ,isDarkMode, setIsDarkMode}) => {
 console.log(isDarkMode)
 
   return (
  <>
- <p className="text-red-400 dark:text-dark-primary">hello</p>
+<Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+{children}
+<Footer setIsDarkMode={setIsDarkMode}/>
  </>
   )
 }
