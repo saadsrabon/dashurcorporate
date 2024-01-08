0c824d0bc3a8914b67c9a80c377fd9b35bf9c84a
Saad Srabon <saadsrabon2@gmail.com>
2024-01-09 01:41:43 +0600

    function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { MainLayout } from "./Layout/MainLayout"
+import { useEffect, useState } from "react";
 
 
 function App() {
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // const toggleTheme = () => {
+  //   setIsDarkMode(!isDarkMode);
+  // };
+
+  useEffect(() => {
+    const currentTime = new Date().getHours();
+
+    // Assuming night is from 8 PM to 6 AM, adjust the timings as needed
+    const isNightTime = currentTime >= 20 || currentTime < 6;
+
+    setIsDarkMode(isNightTime);
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
 
   return (
-    <>
-  
-       
-    </>
+    <BrowserRouter>
+    <MainLayout 
+    isDarkMode={isDarkMode}
+    setIsDarkMode={setIsDarkMode}
+
+    >
+      <Routes>
+        {/* <Route path="/" element={<Jobs/>} />
+        <Route path="/addjobs" element={<AddJob/>} />
+         */}
+      </Routes>
+    </MainLayout>
+    </BrowserRouter>
   )
 }
 
