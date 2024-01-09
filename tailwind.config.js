5cd371300606ab347592e0c464bce427cb14bb56
Saad Srabon <saadsrabon2@gmail.com>
2024-01-10 05:24:02 +0600

    base color changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,7 +46,18 @@ export default {
 
   plugins: [require("daisyui")],
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      {
+        mytheme: {
+        
+
+        
+"base-100": "#024666",
+        
+
+        },
+      },
+    ] , // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: false, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
