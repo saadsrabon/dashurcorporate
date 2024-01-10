9efdfcebdc0f43b525780ca4a6fb21bdcd2edfc1
Saad Srabon <saadsrabon2@gmail.com>
2024-01-10 23:39:04 +0600

    added slider

--- src/components/basicSlider.jsx
+++ src/components/basicSlider.jsx
@@ -11,13 +11,15 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
+import TechnologiesCard from './technologiesCard';
 
 export default function App() {
   return (
-    <>
+    <div className='py-16'>
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
+        
         loop={true}
         pagination={{
           clickable: true,
@@ -26,16 +28,29 @@ export default function App() {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide
+        style={{background:"none"}}
+        >
+            <TechnologiesCard/>
+        </SwiperSlide>
+        <SwiperSlide
+        style={{background:"none"}}
+        >
+            <TechnologiesCard/>
+        </SwiperSlide>
+        <SwiperSlide
+        style={{background:"none"}}
+        >
+            <TechnologiesCard/>
+        </SwiperSlide>
+        <SwiperSlide
+        style={{background:"none"}}
+        >
+            <TechnologiesCard/>
+        </SwiperSlide>
+       
+       
       </Swiper>
-    </>
+    </div>
   );
 }
---
+++ src/components/technologiesCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const TechnologiesCard = () => {
+  return (
+    <div className='bg-[#26262633] py-5 px-2'>
+            <div className='flex justify-center items-center'>
+                <h2 className='uppercase'>Front-END DevelopmEnt</h2>
+                </div>
+                <div className='flex justify-center items-center'>
+                <p className='text-light-text text-base leading-6  dark:text-[#D2D2D2]'>
+                We build responsive, fast, and scalable web applications that work seamlessly across all devices.
+                </p>
+                </div>
+    
+    
+    </div>
+    
+  )
+}
+
+export default TechnologiesCard;
