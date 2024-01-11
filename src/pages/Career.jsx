ce5f7c6c2ae17f912f58b03b9f55485f1c77e3a8
Saad Srabon <saadsrabon2@gmail.com>
2024-01-11 23:01:35 +0600

    button desing added

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -7,13 +7,13 @@ const Career = () => {
         {
           jobData.map((job, index) => (
             <CardWrapper key={index}>
-            <div>
-              <h2>{job?.jobtitle}</h2>
+            <div className="mb-8">
+              <h2 className="font-sans text-2xl font-bold ">{job?.jobtitle} ({job?.jobType})</h2>
               <p>{job?.description}</p>
             </div>
-            <div className="flex justify-center space-x-6">
-              <button>Submit CV/Resume</button>
-              <button>View Job Description</button>
+            <div className="flex justify-center space-x-6 my-4">
+              <button className="px-3 py-2 text-[#0C0C0C] text-base font-medium rounded-lg bg-light-primary">Submit CV/Resume</button>
+              <button className="px-2 py-3 text-light-primary text-base font-medium rounded-lg bg-transparent border-light-primary border-2" >View Job Description</button>
             </div>
           </CardWrapper>
           ))
