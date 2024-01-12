09ba93c0741fc913afc8af3045727801eeb240c2
Saad Srabon <saadsrabon2@gmail.com>
2024-01-13 02:44:38 +0600

    vison added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 
 import Chips from "../components/chips"
 import aboutBanner from "../assets/about.png"
+import CardWrapper from './../components/CardWrapper';
 
 const About = () => {
   return (
@@ -18,6 +19,25 @@ const About = () => {
         <h2 className="text-light-text text-left py-4 text-5xl uppercase font-sans font-bold  dark:text-dark-text">Connecting Dreams, Crafting Realities</h2>
     </div>
               <p className="text-light-text dark:text-dark-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat ipsum odit cupiditate nostrum delectus a, asperiores sequi nesciunt ullam eius, quis molestiae eos labore voluptatem officiis. Tenetur, eos, maiores odit optio, fuga provident delectus nam est laudantium assumenda saepe consequatur.</p>     
+      </div>
+    </div>
+    {/* Mission and vison Part */}
+
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 my-24">
+        <CardWrapper >
+        <div >
+        <h2 className="'text-light-text dark:text-dark-text text-2xl font-bold mb-4 ">Our Mission</h2>
+          <p className="text-light-text dark:text-dark-text">Provide outstanding service, high-end applications and products, which allow our clients to grow and enhance their brand(s).</p>
+        </div>
+        </CardWrapper>
+        <CardWrapper >
+        <div >
+        <h2 className="'text-light-text dark:text-dark-text text-2xl font-bold mb-4 ">Our Vision</h2>
+          <p className="text-light-text dark:text-dark-text">Provide outstanding service, high-end applications and products, which allow our clients to grow and enhance their brand(s).</p>
+        </div>
+        </CardWrapper>
+        
       </div>
     </div>
     </div>
