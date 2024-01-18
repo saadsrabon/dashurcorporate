69d1456aa9d3b28ecf11e62d770f98dd37375603
Saad Srabon <saadsrabon2@gmail.com>
2024-01-19 04:48:52 +0600

    all pages responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ import CardWrapper from './../components/CardWrapper';
 
 const About = () => {
   return (
-    <div className="w-[80%] mx-auto my-24">
+    <div className="w-[80%] mx-auto my-12 lg:my-24">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
            <div>
             <img src={aboutBanner}alt="" />
@@ -16,7 +16,7 @@ const About = () => {
               <Chips title="About Us"/>
               </div>
               <div className="flex  items-center ">
-        <h2 className="text-light-text text-left py-4 text-5xl uppercase font-sans font-bold  dark:text-dark-text">Connecting Dreams, Crafting Realities</h2>
+        <h2 className="text-light-text text-left py-4 text-[24px] md:text-5xl uppercase font-sans font-bold  dark:text-dark-text">Connecting Dreams, Crafting Realities</h2>
     </div>
               <p className="text-light-text dark:text-dark-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat ipsum odit cupiditate nostrum delectus a, asperiores sequi nesciunt ullam eius, quis molestiae eos labore voluptatem officiis. Tenetur, eos, maiores odit optio, fuga provident delectus nam est laudantium assumenda saepe consequatur.</p>     
       </div>
@@ -24,7 +24,7 @@ const About = () => {
     {/* Mission and vison Part */}
 
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 my-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-8 lg:gap-20 my-24">
         <CardWrapper >
         <div >
         <h2 className="text-light-text dark:text-dark-text text-2xl font-bold mb-4 ">Our Mission</h2>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,13 +4,13 @@ import Chips from "../components/chips"
 
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center w-[80%] mx-auto my-24">
+    <div className="flex flex-col items-center w-[95%] mx-auto my-24">
           <div className="flex flex-col  space-y-4 justify-center items-center">
               <div className="flex ">
               <Chips title="Contact Us"/>
               </div>
-              <div className="flex  flex-col items-center w-2/4 ">
-        <h2 className="text-light-text text-center leading-snug py-4 text-5xl uppercase font-sans font-bold  dark:text-dark-text">Connecting Dreams, Crafting Realities</h2>
+              <div className="flex  flex-col items-center w-3/4 md:w-2/4 lg:w-[40%]">
+        <h2 className="text-light-text text-center leading-snug md:leading-normal py-4 text-[24px] md:text-5xl uppercase font-sans font-bold  dark:text-dark-text">Connecting Dreams, Crafting Realities</h2>
         <p className="text-light-text dark:text-dark-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat ipsum odit cupiditate nostrum delectus a, asperiores sequi nesciunt ullam eius, quis molestiae eos labore voluptatem officiis. Tenetur, eos, maiores odit optio, fuga provident delectus nam est laudantium assumenda saepe consequatur.</p>     
     </div>
              
@@ -35,15 +35,15 @@ const Contact = () => {
           </div>
         </CardWrapper>
         <CardWrapper>
-          <div className="flex justify-between space-x-8">
-            <div>
+          <div className="flex justify-between md:space-x-8">
+            <div className="flex-grow-0">
               <img src="" alt="" />
               <div>
               <h2 className="text-light-text dark:text-dark-text font-bold ">Let us contact </h2>
               <p className="text-light-text dark:text-dark-text">Provide your information to connect</p>
               </div>
             </div>
-            <div>
+            <div className="shrink-0">
               <button className="px-3 py-2 text-[#0C0C0C] text-base font-medium rounded-lg bg-light-primary">
               Get In Touch
               </button>
