e8baf89d8ab0d02043a48d600a9ef942231c1d9c
Saad Srabon <saadsrabon2@gmail.com>
2024-01-20 04:20:54 +0600

    Description added

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import CardWrapper from "../components/CardWrapper";
 import BasicTitle from "../components/basicTitle";
 import Chips from "../components/chips";
 import jobData from '../utils/job.json';
 
 const Career = () => {
+  const [showJob, setShowJob] = useState(false);
   return (
     <div className="w-full md:w-4/5 lg:w-3/4 xl:w-2/3 mx-auto my-12 flex-col items-center px-4">
       <div className="flex flex-col justify-center items-center">
@@ -16,11 +18,11 @@ const Career = () => {
           <CardWrapper key={index}>
             <div className="mb-8 ">
               <h2 className="font-sans text-2xl font-bold text-light-text dark:text-dark-text ">{job?.jobtitle} ({job?.jobType})</h2>
-              <p className="my-2  text-light-text dark:text-dark-text">{job?.description}</p>
+              {showJob?<p className="my-2  text-light-text dark:text-dark-text">{job?.description}</p>:""}
             </div>
             <div className="flex flex-col md:flex-row justify-center md:space-x-6 mt-4">
               <button className="mb-2 md:mb-0 px-3 py-2 text-[#0C0C0C] text-base font-medium rounded-lg bg-light-primary">Submit CV/Resume</button>
-              <button className="px-2 py-3 text-light-primary text-base font-medium rounded-lg bg-transparent border-light-primary border-2" >View Job Description</button>
+              <button onClick={()=>setShowJob(prev=>!prev)} className="px-2 py-3 text-light-primary text-base font-medium rounded-lg bg-transparent border-light-primary border-2" >View Job Description</button>
             </div>
           </CardWrapper>
         ))
