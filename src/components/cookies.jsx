5e4b43f9666d05f87196f18d6cb6a9099bc3c6d2
Saad Srabon <saadsrabon2@gmail.com>
2024-01-19 23:06:17 +0600

    cookies design added

---
+++ src/components/cookies.jsx
@@ -0,0 +1,17 @@
+
+
+export const Cookies = () => {
+  return (
+  
+
+<dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    
+  </div>
+</dialog>
+  )
+}
