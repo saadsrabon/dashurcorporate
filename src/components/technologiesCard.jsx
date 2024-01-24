02ab41cc12741cce2375e670e088613ae9c2a079
Saad Srabon <saadsrabon2@gmail.com>
2024-01-24 23:17:31 +0600

    2024-01-24 23:17:31 - technologies section added

--- src/components/technologiesCard.jsx
+++ src/components/technologiesCard.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+
 
 const TechnologiesCard = ({item}) => {
     const {title, description, technology} = item;
+    const [tooltipOpen, setTooltipOpen] = useState(false);
   return (
     <div className='techCard py-8 px-4 rounded-lg border border-solid border-white border-opacity-20 dark:bg-gray-800 dark:bg-opacity-20 bg-white bg-opacity-20 shadow-xl my-16 ">
     '>
@@ -18,19 +21,21 @@ const TechnologiesCard = ({item}) => {
               <div className='grid grid-cols-6  py-8 place-items-center'>
               {
                 technology?.map((item,index) => (
-                  <div className="relative" key={index}>
+                  <div onMouseLeave={()=>setTooltipOpen(null)} onMouseEnter={()=>setTooltipOpen(index)} className="relative" key={index}>
                     <div  className='flex  mb-2    space-x-2 items-center border border-white border-opacity-20 px-3.5 py-2 rounded-lg' data-tip="hello world fgkhskfghksfhg">
                    <div className="w-8 ">
                    <img className="w-[100%]" src={item?.imageLink} alt="" />
                    </div>
                     <h2  className="text-light-text  dark:text-dark-text">{item?.name}</h2>
                 </div>
-                <div className="absolute bottom-100   w-[300px] ">
-                <div className="relative border space-x-2">
-                <span className={`w-0 h-0 border-[#02A9F7] border-r-[20px] border-b-[20px] border-r-transparent absolute rotate-[136deg] left-10 top-4`}></span>
-                <p>{item?.details}</p>
-                </div>
-                </div>
+                {
+                  tooltipOpen == index &&(<div className="absolute bottom-100 -left-36   w-[300px] ">
+                  <div className="relative border-[556D78] border space-x-2 bg-[#556D78]">
+                  <span className={`w-0 h-0 border-[#556D78] border-r-[20px] border-b-[20px] border-r-transparent absolute rotate-[136deg] left-40 -top-2`}></span>
+                  <p>{item?.details}</p>
+                  </div>
+                  </div>)
+                }
                
                 </div>
                 ))
