2cc19fe03cdcdcd9c6e3c8b65085125722dfeda3
Saad Srabon <saadsrabon2@gmail.com>
2024-01-24 18:29:29 +0600

    2024-01-24 18:29:28 - border color fixed

--- src/components/JobDescription.jsx
+++ src/components/JobDescription.jsx
@@ -5,14 +5,14 @@ const JobDescription = ({job}) => {
     console.log(job)
   return (
     <div>
-        <p className='my-2 font-semibold'>Job Overview</p>
-        <p className='mb-4'>{job?.overview}</p>
-        <p className='my-2 font-bold'>Responsibilities for  {job?.jobtitle}</p>
-        <ul className='list-disc ml-6'>
+        <p className='my-2 font-semibold text-light-text dark:text-dark-text'>Job Overview</p>
+        <p className='mb-4 text-light-text dark:text-dark-text'>{job?.overview}</p>
+        <p className='my-2 font-bold text-light-text dark:text-dark-text'>Responsibilities for  {job?.jobtitle}</p>
+        <ul className='list-disc ml-6 text-light-text dark:text-dark-text'>
             {job?.responsibilities.map((item) => (<li className='' key={item}>{item}</li>))}
         </ul>
-        <p className='my-4 font-bold'>Qualifications for {job?.jobtitle}</p>
-        <ul className='list-disc ml-6'>
+        <p className='my-4 font-bold text-light-text dark:text-dark-text'>Qualifications for {job?.jobtitle}</p>
+        <ul className='list-disc ml-6 text-light-text dark:text-dark-text'>
         {job?.qualification.map((item) => (<li key={item}>{item}</li>))}
         </ul>
     </div>
--- src/components/UploadResume.jsx
+++ src/components/UploadResume.jsx
@@ -35,18 +35,18 @@ const UploadResume = () => {
   
     return (
       <div className="w-full">
-         <div className="rounded-lg border border-solid border-white border-opacity-20 dark:bg-gray-800 dark:opacity-100 bg-white bg-opacity-20 shadow-xl px-6 py-10 my-8">
+         <div className="rounded-lg border border-solid border-white border-opacity-20  dark:bg-gray-800 dark:opacity-100 bg-white bg-opacity-100 shadow-xl px-6 py-10 my-8">
        <form className="">
         <div className="mb-6" >
-          <label className="block" htmlFor="name">Name</label>
+          <label className="block text-light-text dark:text-dark-text" htmlFor="name">Name</label>
           <input className="appearance-none bg-transparent w-full py-2 outline-none border-b-[0.5px] border-[#454545]" type="text" name="name" />
         </div>
         <div className="mb-6">
-          <label className="block" htmlFor="phone">Phone</label>
+          <label className="block text-light-text dark:text-dark-text" htmlFor="phone">Phone</label>
           <input className="appearance-none bg-transparent w-full py-2 outline-none border-b-[0.5px] border-[#454545]" type="text" name="phone" />
         </div>
         <div className="mb-6">
-          <label className="block" htmlFor="email">Email</label>
+          <label className="block text-light-text dark:text-dark-text" htmlFor="email">Email</label>
           <input className="appearance-none bg-transparent w-full py-2 outline-none border-b-[0.5px] border-[#454545]" type="email" name="email" />
         </div>
       <div className="file-upload" onDrop={handleDrop} onDragOver={handleDragOver}>
@@ -55,9 +55,9 @@ const UploadResume = () => {
      
      
        
-      <div className="flex flex-col items-center justify-center py-5 px-4 border-white border-opacity-20 rounded-lg border ">
-      <p>Upload or drop your resume in the Word, Google Doc, PDF format</p>
-      {selectedFile && <p>Selected file: {selectedFile.name}</p>}
+      <div className="flex flex-col items-center justify-center py-5 px-4 border-black border-opacity-20 dark:border-white dark:border-opacity-20 rounded-lg border ">
+      <p className="text-light-text dark:text-dark-text">Upload or drop your resume in the Word, Google Doc, PDF format</p>
+      {selectedFile && <p className="text-light-text dark:text-dark-text">Selected file: {selectedFile.name}</p>}
       <button onClick={handleUpload} className="mb-2 mt-4  md:mb-0 px-3 py-2 text-[#0C0C0C] text-base font-medium rounded-lg bg-light-primary" >View Job Description</button>
       </div>
 
