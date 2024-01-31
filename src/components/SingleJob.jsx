5e85416793e3e1db1bd20fe54a57377d060bbc4f
Saad Srabon <saadsrabon2@gmail.com>
2024-02-01 01:33:20 +0600

    2024-02-01 01:33:20 - Solved the issue of Form is not closing in different states

--- src/components/SingleJob.jsx
+++ src/components/SingleJob.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable react/prop-types */
 
-import {  useState } from 'react';
+import { useState } from 'react';
 import CardWrapper from './CardWrapper'
 import JobDescription from './JobDescription'
 import UploadResume from './UploadResume'
 
 
-const SingleJob = ({job ,handleOpen ,isActive}) => {
-  const [openResume, setOpenResume] = useState(0);
- 
+const SingleJob = ({job, handleOpen, handleClose, isActive, index }) => {
+  const [openResume, setOpenResume] = useState(false);
 
+  const handleResumeClick = () => {
+    setOpenResume(!openResume);
+    if (!openResume) {
+      handleOpen(index);
+    } else {
+      handleClose();
+    }
+  };
   return (
   
     <CardWrapper>
@@ -19,12 +26,14 @@ const SingleJob = ({job ,handleOpen ,isActive}) => {
       {isActive  ?<JobDescription job={job}/>:""}
     </div>
     <div className="flex flex-col md:flex-row justify-center md:space-x-6 mt-4">
-      <button onClick={()=>setOpenResume(job?.id)} className="mb-2 md:mb-0 px-3 py-2 text-[#0C0C0C] text-base font-medium rounded-lg bg-light-primary">Submit CV/Resume</button>
+      <button onClick={handleResumeClick} className="mb-2 md:mb-0 px-3 py-2 text-[#0C0C0C] text-base font-medium rounded-lg bg-light-primary">Submit CV/Resume</button>
       <button onClick={handleOpen} className="px-2 py-3 text-light-primary text-base font-medium rounded-lg bg-transparent border-light-primary border-2" >View Job Description</button>
     </div>
-{openResume ==job.id && <div className='absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]'>
-      <UploadResume setOpenResume={setOpenResume}/>
-    </div>}
+    {openResume && isActive && (
+      <div className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
+        <UploadResume />
+      </div>
+    )}
     
     </div>
   </CardWrapper>
--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -8,35 +8,38 @@ import jobData from '../utils/job.json';
 import SingleJob from "../components/SingleJob";
 
 const Career = () => {
-  const [indexData, setIndexData] = useState(0);
-  const [tabIndex , setTabIndex] = useState(0);
+  const [openJobIndex, setOpenJobIndex] = useState(null);
+  const [tabIndex, setTabIndex] = useState(0);
 
+  const handleOpen = (index) => {
+    setOpenJobIndex(index);
+  };
 
-const handleOpen = (index) => {
-  setIndexData(index)
-}
   return (
     <div className="w-full md:w-4/5 lg:w-3/4 xl:w-2/3 mx-auto my-12 mt-48 flex-col items-center px-4">
       <div className="flex flex-col justify-center items-center">
         <Chips title="Careers" />
         <BasicTitle width="w-full md:w-[94%] lg:w-[60%]" title="Make an Outstanding career with dashur AI" />
-        <p className="text-center md:text-left md:ml-[-45px]">Your feedback and inquiries are important to us, and we're here to <br /> provide the support you need.</p>
+        <p className="text-center md:text-left md:ml-[-45px]">
+          Your feedback and inquiries are important to us, and we're here to
+          <br /> provide the support you need.
+        </p>
       </div>
-       {/* Tab Version */}
-       <div role="tablist" className="tabs tabs-lifted my-12">
-  <a onClick={()=>setTabIndex(0)} role="tab" className={`${tabIndex==0?"tab tab-active font-bold  dark:text-dark-text":"tab text-light-text dark:text-dark-text"}`}>Job Positions</a>
-  <a onClick={()=>setTabIndex(1)} role="tab" className={`${tabIndex==1?"tab tab-active font-bol  dark:text-dark-text":"tab text-light-text dark:text-dark-text"}`}>Learning Lab</a>
-  
-
-</div>
-      {
-       tabIndex==0?( jobData.map((job, index) => (
-        
-          <SingleJob key={index} job={job} handleOpen={()=>handleOpen(index)} isActive={indexData==index}/>
-        ))) : ("")
-      }
+      {/* Tab Version */}
+      <div role="tablist" className="tabs tabs-lifted my-12">
+        <a onClick={() => setTabIndex(0)} role="tab" className={`${tabIndex === 0 ? "tab tab-active font-bold  dark:text-dark-text" : "tab text-light-text dark:text-dark-text"}`}>
+          Job Positions
+        </a>
+        <a onClick={() => setTabIndex(1)} role="tab" className={`${tabIndex === 1 ? "tab tab-active font-bol  dark:text-dark-text" : "tab text-light-text dark:text-dark-text"}`}>
+          Learning Lab
+        </a>
+      </div>
+      {tabIndex === 0 &&
+        jobData.map((job, index) => (
+          <SingleJob key={index} job={job} handleOpen={() => handleOpen(index)} index={index}  isActive={index === openJobIndex} />
+        ))}
     </div>
   );
-}
+};
 
 export default Career;
