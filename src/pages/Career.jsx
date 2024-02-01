137063e067ce1af246f398025b7cbdc0594a2006
Saad Srabon <saadsrabon2@gmail.com>
2024-02-02 04:29:37 +0600

    2024-02-02 04:29:37 - tabs removed

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -20,19 +20,17 @@ const Career = () => {
       <div className="flex flex-col justify-center items-center">
         <Chips title="Careers" />
         <BasicTitle width="w-full md:w-[94%] lg:w-[60%]" title="Make an Outstanding career with dashur AI" />
-        <p className="text-center md:text-left md:ml-[-45px]">
+        {/* <p className="text-center md:text-left md:ml-[-45px]">
           Your feedback and inquiries are important to us, and we're here to
           <br /> provide the support you need.
-        </p>
+        </p> */}
       </div>
       {/* Tab Version */}
-      <div role="tablist" className="tabs tabs-lifted my-12">
-        <a onClick={() => setTabIndex(0)} role="tab" className={`${tabIndex === 0 ? "tab tab-active font-bold  dark:text-dark-text" : "tab text-light-text dark:text-dark-text"}`}>
+      <div role="tablist" className="tabs my-12 bg-l">
+        <a onClick={() => setTabIndex(0)} role="tab" className={`${tabIndex === 0 ? "tab uppercase tab-active font-bold text-3xl dark:text-dark-text" : "tab text-light-text dark:text-dark-text "}`}>
           Job Positions
         </a>
-        <a onClick={() => setTabIndex(1)} role="tab" className={`${tabIndex === 1 ? "tab tab-active font-bol  dark:text-dark-text" : "tab text-light-text dark:text-dark-text"}`}>
-          Learning Lab
-        </a>
+       
       </div>
       {tabIndex === 0 &&
         jobData.map((job, index) => (
