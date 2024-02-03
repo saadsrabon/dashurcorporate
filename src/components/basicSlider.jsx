bf0f0cdab87719fdd28dee25568bdcf2640dc213
Saad Srabon <saadsrabon2@gmail.com>
2024-02-04 01:01:30 +0600

    2024-02-04 01:01:30 - slick slider initialized

--- src/components/basicSlider.jsx
+++ src/components/basicSlider.jsx
@@ -1,57 +1,37 @@
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+
+import Slider from "react-slick";
+
 import db from  '../utils/technologies.json'
 // import required modules
-import { Pagination, Navigation ,Autoplay ,Mousewheel } from 'swiper/modules';
+
 import TechnologiesCard from './technologiesCard';
 
 
 export default function App() {
-   
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <div className=''>
         
-      <Swiper
-       style={{'--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff', 'height':'550px', margin:"20px 0",  overflow:'hidden'}}
-        direction={'vertical'}
-        slidesPerView={1}
-        spaceBetween={30}
-        mousewheel={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Mousewheel, Pagination]}
-        className="mySwiper"
-        // breakpoints={
-        //     {
-        //         320: {
-        //             slidesPerView: 1,
-        //             spaceBetween: 10
-        //         },
-        //         768: {
-        //             slidesPerView: 1,
-        //             spaceBetween: 20
-        //         },
-        //         1024: {
-        //             slidesPerView: 1,
-        //             spaceBetween: 30
-        //         }
-        //     }
-        // }
+      <Slider
+       {...settings}
+        
       >
         {
             db.map((item, index) => (
-                <SwiperSlide data  key={index}>
-                    <TechnologiesCard item={item} />
-                </SwiperSlide>
+                
+                    <TechnologiesCard key={index} item={item} />
+                
             ))
         }
-      </Swiper>  
+      </Slider>  
     </div>
   );
 }
