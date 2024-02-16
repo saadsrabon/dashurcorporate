eb655920506d9aef9dc48a7a0a100092797286a1
Saad Srabon <saadsrabon2@gmail.com>
2024-02-16 22:56:22 +0600

    2024-02-16 22:56:22 - added Service Widget

---
+++ src/chatBotWidgets/ServiceWidget.jsx
@@ -0,0 +1,21 @@
+
+
+const ServiceWidget = () => {
+  return (
+    <div>
+        <h2>Our Services</h2>
+    <ul className="ml-6 list-disc">
+        <li>GRAPHIC DESIGN</li>
+        <li>WEB DEVELOPMENT</li>
+        <li>UI/UX DESIGN</li>
+        <li>APP DEVELOPMENT</li>
+        <li>DIGITAL MARKETING</li>
+        <li>AI INTEGRATION</li>
+        <li>SEO</li>
+       
+    </ul>
+</div>
+  )
+}
+
+export default ServiceWidget
--- src/chatBotWidgets/startBtn.jsx
+++ src/chatBotWidgets/startBtn.jsx
@@ -4,10 +4,11 @@
 const StartBtn = (props) => {
     const {handleAbout ,handleMarkets} = props.actions;
   return (
-    <div className="flex space-x-2 ml-2">
-    <button onClick={()=>handleAbout()} className='px-3 py-1 border border-light-primary'>About Us</button>
-    <button onClick={()=>handleMarkets()} className='px-3 py-1 border border-light-primary'>Markets</button>
-    <button className='px-4 py-2 border border-light-primary'>Services</button>
+    <div className="grid space-y-4 ">
+    <button onClick={()=>handleAbout()} className='px-3 py-1 border border-light-primary hover:text-light-primary   rounded-lg'>About</button>
+    <button onClick={()=>handleMarkets()} className='px-3 py-1 border border-light-primary hover:text-light-primary  rounded-lg'>Markets</button>
+    <button className='px-2 py-1 border border-light-primary hover:text-light-primary  rounded-lg'>Services</button>
+    <button className='px-3 py-1 border border-light-primary hover:text-light-primary  rounded-lg'>Contacts</button>
   </div>
   )
 }
